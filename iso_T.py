5e769050c5954d14b3ca9897a7916392ab5625de
Mark Nixon <markwnixon@gmail.com>
2019-12-24 09:48:23 -0500

    New Display Panel

--- iso_T.py
+++ iso_T.py
@@ -1837,7 +1837,8 @@ def isoT():
                                Description=myo.Description, Chassis=myo.Chassis, Detention='0', Storage='0', Release=0, Shipper=myo.Shipper,
                                Type=myo.Type, Time3=None, Bid=myo.Bid, Lid=myo.Lid, Did=myo.Did, Label=myo.Label, Dropblock1=myo.Dropblock1,
                                Dropblock2=myo.Dropblock2, Commodity=myo.Commodity,Packing=myo.Packing, Links=myo.Links, Hstat=-1,
-                               Istat=-1,Proof=None,Invoice=None,Gate=None,Package=None,Manifest=None)
+                               Istat=-1,Proof=None,Invoice=None,Gate=None,Package=None,Manifest=None,Scache=0,Pcache=0,
+                               Icache=0,Mcache=0,Pkcache=0)
                 db.session.add(input)
                 db.session.commit()
             else:
